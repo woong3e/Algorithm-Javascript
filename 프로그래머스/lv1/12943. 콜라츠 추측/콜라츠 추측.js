5afc4c8e3734dfeb85d6f7ed703dd10e1f8cd2b0
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-05 17:16:36 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,7 +1,7 @@
 function solution(num) {
     let answer = 0;
+    if(num ===1) return 0;
     for(;;){
-        if(num ===1) return 0;
         answer++;
         if(answer > 500) return -1;
         num % 2 === 0 ? num = num/2 : num = num * 3 + 1;
