bb3867329a3d3ff40c102ea258aa4eb298641d5d
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-07 09:28:23 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,8 @@
 function solution(seoul) {
     let answer = '';
-    for(let i =0;i<seoul.length;i++){
-        if(seoul[i] === 'Kim') {answer = '김서방은 '+ i + "에 있다"}
-    }
+    // for(let i =0;i<seoul.length;i++){
+    //     if(seoul[i] === 'Kim') {answer = '김서방은 '+ i + "에 있다"}
+    // }
+    answer = "김서방은 " + (seoul.indexOf("Kim")) + "에 있다";
     return answer;
 }
