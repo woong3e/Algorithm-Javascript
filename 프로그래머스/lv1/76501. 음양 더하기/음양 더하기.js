3885d41772f49c35a69b0e37985e50d204dd07f4
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-07 14:58:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,14 @@
 function solution(absolutes, signs) {
     let answer = 0;
-    let newArr = [];
-    for(let i =0; i<signs.length;i++){
-        signs[i]===true ? newArr.push(absolutes[i]) : newArr.push(-absolutes[i]);
-    }
-    for(el of newArr){
-        answer += el;
-     }
+    // let newArr = [];
+    // for(let i =0; i<signs.length;i++){
+    //     signs[i]===true ? newArr.push(absolutes[i]) : newArr.push(-absolutes[i]);
+    // }
+    // for(el of newArr){
+    //     answer += el;
+    //  } 
+    
+    //reduce 메서드를 사용한 풀이
+    answer = absolutes.reduce((acc,curr,i) => acc + curr * (signs[i] ? 1 : -1), 0);
     return answer;
 }
