c20517ae4510f21463b584d23d4b33ee851f3933
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-11 15:45:42 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,5 +1,7 @@
 function solution(s) {
     let answer = true;
-s.length === 4 || s.length === 6 ? answer = s.split('').every(el => !isNaN(el)) : answer = false;
+    s.length === 4 || s.length === 6
+    ? answer = s.split('').every(el => !isNaN(el))
+    : answer = false;
     return answer;
 }
