a22ae6a6a69d6aa2f3d34b77379e91905a8e52fb
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-17 12:35:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.79 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,7 +1,10 @@
 function solution(arr) {
-    let answer = [];
-    for(let i=0;i<arr.length;i++){
-        (arr[i] !== arr[i + 1]) && answer.push(arr[i]);
-    }
+    // let answer = [];
+    // for(let i=0;i<arr.length;i++){
+    //     (arr[i] !== arr[i + 1]) && answer.push(arr[i]);
+    // }
+    
+    //필터를 쓴다면 아래와 같이 쓸 수 있겠다.
+    let answer = arr.filter((el,idx) => el !==arr[idx+1]);
     return answer;
 }
