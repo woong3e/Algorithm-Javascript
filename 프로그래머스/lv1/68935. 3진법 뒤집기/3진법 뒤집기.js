c2194e0a397e02e7c222e36f95c2b9023d7364c8
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-17 19:43:41 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,6 +1,9 @@
 function solution(n) {
     //10진법을 3진법으로 바꾸고 뒤집고 10진법으로 다시 바꾸어주기
-    let answer = 0;
-    answer = parseInt((n.toString(3).split('').reverse().join('')),3);
+    // let answer = 0;
+    // answer = parseInt((n.toString(3).split('').reverse().join('')),3);
+    
+    //spread operator를 사용한 풀이법
+    answer = parseInt([...n.toString(3)].reverse().join(''),3);
     return answer;
 }
