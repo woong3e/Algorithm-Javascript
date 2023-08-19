c3aefa6963b2cda9c2f557a30725b7686c9723bc
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-19 12:44:20 +0900

    [level 1] Title: 예산, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -2,6 +2,7 @@ function solution(d, budget) {
     //조건 1)최대한 많은 요소 갯수를 더해야 함.
     //조건 2)그 요소들의 합이 budget보다 작아야함.
     //think... 일단 d를 작은순으로 정렬해야겠다.
+    //입력:[1,1,1,1,1],10 출력:5 인 경우를 고려하여 if(sum < budget) return arr.length; 추가해주었다.
     let answer = 0;
     let sum = 0;
     let arr = [];
@@ -9,7 +10,6 @@ function solution(d, budget) {
     for(let i = 0;i<d.length;i++){
         sum += d[i];
         arr.push(d[i]);
-        // console.log(`i: ${i},sum: ${sum} arr: ${arr}`);
         if(sum>budget) return arr.length-1;
         if(sum===budget) return arr.length;
         
