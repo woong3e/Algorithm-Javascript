c979da886a5da4ec4e0e606220d90ece54b55490
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-22 16:01:47 +0900

    [level 1] Title: 최소직사각형, Time: 5.51 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,13 +1,22 @@
 function solution(sizes) {
-    let widthArr = [];
-    let width = 0;
-    let lengthArr = [];
-    sizes.map(el => el.sort((a,b) => a-b));
-    for(let i=0;i<sizes.length;i++){
-      widthArr.push(sizes[i][0]);
-      lengthArr.push(sizes[i][1]);
-    }
-  width = Math.max(...widthArr);
-  length = Math.max(...lengthArr);
-  return width * length;
+  //   let widthArr = [];
+  //   let width = 0;
+  //   let lengthArr = [];
+  //   sizes.map(el => el.sort((a,b) => a-b));
+  //   for(let i=0;i<sizes.length;i++){
+  //     widthArr.push(sizes[i][0]);
+  //     lengthArr.push(sizes[i][1]);
+  //   }
+  // width = Math.max(...widthArr);
+  // length = Math.max(...lengthArr);
+const arr = sizes.map((el) => {
+    const [a, b] = el;
+    return {
+      width: Math.max(a, b),
+      height: Math.min(a, b)
+    };
+  });
+const width = Math.max(...arr.map(el => el.width));
+const height = Math.max(...arr.map(el => el.height));
+  return width * height;
 }
