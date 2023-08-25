ff96e9b6559121dab59bc869e8a59d5d043f5021
woong3e <86347667+woong3e@users.noreply.github.com>
2023-08-25 15:37:05 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,13 +1,25 @@
-function solution(s) {
-    s = s.replace(/zero/gi,'0')
-    s = s.replace(/one/gi,'1')
-    s = s.replace(/two/gi,'2')
-    s = s.replace(/three/gi,'3')
-    s = s.replace(/four/gi,'4')
-    s = s.replace(/five/gi,'5')
-    s = s.replace(/six/gi,'6')
-    s = s.replace(/seven/gi,'7')
-    s = s.replace(/eight/gi,'8')
-    s = s.replace(/nine/gi,'9')
-    return parseInt(s);
+//정규식풀이
+//function solution(s) {
+//     s = s.replace(/zero/gi,'0')
+//     s = s.replace(/one/gi,'1')
+//     s = s.replace(/two/gi,'2')
+//     s = s.replace(/three/gi,'3')
+//     s = s.replace(/four/gi,'4')
+//     s = s.replace(/five/gi,'5')
+//     s = s.replace(/six/gi,'6')
+//     s = s.replace(/seven/gi,'7')
+//     s = s.replace(/eight/gi,'8')
+//     s = s.replace(/nine/gi,'9')
+//     return parseInt(s);
+// }
+
+//비정규식풀이
+const solution = s => {
+  let strArr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let answer = s;
+    for(let i=0;i<strArr.length;i++){
+      let arr = answer.split(strArr[i]);
+      answer = arr.join(i);
+    }
+  return Number(answer);
 }
